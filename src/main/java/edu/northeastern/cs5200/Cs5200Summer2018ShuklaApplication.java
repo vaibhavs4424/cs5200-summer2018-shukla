d4e1fc6744b6ee1b2bf6c688cfea4f1f521460a3
vaibhavshukla <shukla.v@husky.neu.edu>
2018-05-20 20:01:01 -0400

    First commit - Spring boot project setup

---
+++ src/main/java/edu/northeastern/cs5200/Cs5200Summer2018ShuklaApplication.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Summer2018ShuklaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs5200Summer2018ShuklaApplication.class, args);
+	}
+}
