df10b1fdb073dfc02090a03eb5faa13be5cfaf7a
vaibhavshukla <shukla.v@husky.neu.edu>
2018-06-03 15:52:40 -0400

    Integrating Spring Boot with MySQL

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cs5200-summer2018-shukla
 Spring boot project for course cs5200 (Database Management Systems) at Northeastern College of Computer and Information Scinece
+
+
+## Integrating Spring Boot with MySQL
+
+[Insert a static hello message](http://cs5200-summer2018-shukla.us-east-1.elasticbeanstalk.com/api/hello/insert)
+
+[Insert a parameterized hello message](http://cs5200-summer2018-shukla.us-east-1.elasticbeanstalk.com/api/hello/insert/Some parameterized message)
+
+[Retrieve all hello messages](http://cs5200-summer2018-shukla.us-east-1.elasticbeanstalk.com/api/hello/select/all)
--- src/main/java/edu/northeastern/cs5200/Cs5200Summer2018ShuklaApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Summer2018ShuklaApplication.java
@@ -2,9 +2,20 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Summer2018ShuklaApplication {
+public class Cs5200Summer2018ShuklaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder
+
+			configure(SpringApplicationBuilder application) {
+
+		return application.sources(Cs5200Summer2018ShuklaApplication.class);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Summer2018ShuklaApplication.class, args);
